4425200c0ffa25687c01a8e7bd9af634c4e45423
Lương Trung Kiên <keanane241@gmail.com>
2026-02-10 23:09:57 +0700

    initial

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is running on PORT`, PORT));
