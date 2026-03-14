13fe8e6590abfcf91c92964fbc13a3d26e461acf
Nguyễn Nhật Trường <truongnguyen1211200499@gmail.com>
2026-03-14 16:14:27 +0700

    get cart count

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,6 +5,7 @@ const authMiddleware = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/", authMiddleware, cartController.getCart);
+router.get("/count", authMiddleware, cartController.getCartCount);
 router.post("/add", authMiddleware, cartController.addToCart);
 router.put("/update", authMiddleware, cartController.updateCartItem);
 router.delete("/remove", authMiddleware, cartController.removeFromCart);
